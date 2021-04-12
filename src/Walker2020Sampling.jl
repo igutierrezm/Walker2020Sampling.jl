2e1e21376079d070ebf627311e47e2b64c966706
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-04-12 13:47:58 -0400

    First running version

--- src/Walker2020Sampling.jl
+++ src/Walker2020Sampling.jl
@@ -1,5 +1,46 @@
 module Walker2020Sampling
 
-# Write your package code here.
+import Base:
+    rand
 
+using Random:
+    AbstractRNG
+
+using Distributions:
+    Exponential
+
+export 
+    rand,
+    Walker2020Sampler
+
+Base.@kwdef struct Walker2020Sampler
+    lb::Float64 = -Inf
+    ub::Float64 = +Inf
+    θ::Float64  = 20.0
 end
+
+function rand(
+        rng::AbstractRNG, 
+        s::Walker2020Sampler, 
+        logp::Function, 
+        y0::Float64, 
+        z0::Float64
+    )
+    logw = log(rand(rng)) + logp(y0)
+    l1 = rand(rng) * z0 + (y0 - z0 / 2)
+    z1 = 2 * abs(l1 - y0) + rand(rng, Exponential(s.θ))
+    a1 = max(s.lb, l1 - z1 / 2)
+    b1 = min(s.ub, l1 + z1 / 2)
+    y1 = rand(rng) * (b1 - a1) + a1
+    while logp(y1) < logw
+        if y1 < y0
+            a1 = max(a1, y1)
+        else
+            b1 = min(b1, y1)
+        end
+        y1 = rand(rng) * (b1 - a1) + a1
+    end
+    return y1, z1
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using Walker2020Sampling
+using Distributions
+using Random
 using Test
 
 @testset "Walker2020Sampling.jl" begin
-    # Write your tests here.
+    s = Walker2020Sampler(lb = 0.0, ub = 1.0)
+    rng = MersenneTwister(1)
+    y1, z1 = rand(rng, s, x -> logpdf(Exponential(1.0), x), 1.0, 1.0)
+    @test y1 ≥ 0.0
+    @test y1 ≤ 1.0 
 end
