d7bc0a4f35226f392c1d5f9cc06ec73acb70d09a
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-04-12 16:37:29 -0400

    Increment the number of iterations in the first test

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,7 @@ using Test
     s = Walker2020Sampler(θ = 20.0, lb = -0.5, ub = +0.5)
     rng = MersenneTwister(1)
     y0, z0 = 0.0, 0.1
-    for i = 1:50
+    for i = 1:200
         y0, z0 = rand(rng, s, x -> logpdf(Normal(), x), y0, z0)
         @test y0 ≥ -0.5
         @test y0 ≤ +0.5
