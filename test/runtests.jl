859c60140e26a1fcd9360020abcb548190cdd91e
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-04-12 14:02:44 -0400

    Add more iterations for the tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,10 @@ using Test
 @testset "Walker2020Sampling.jl" begin
     s = Walker2020Sampler(lb = 0.0, ub = 1.0)
     rng = MersenneTwister(1)
-    y1, z1 = rand(rng, s, x -> logpdf(Exponential(1.0), x), 1.0, 1.0)
-    @test y1 ≥ 0.0
-    @test y1 ≤ 1.0 
+    y0, z0 = 1.0, 1.0
+    for i = 1:100
+        y0, z0 = rand(rng, s, x -> logpdf(Exponential(1.0), x), y0, z0)
+        @test y0 ≥ 0.0
+        @test y0 ≤ 1.0 
+    end
 end
