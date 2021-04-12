e45318cfc2331233c217ba0a2c085e7b8c80d22d
Documenter.jl <documenter@juliadocs.github.io>
2021-04-12 16:10:38 +0000

    build based on ebc06ed

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Walker2020Sampling","category":"page"},{"location":"#Walker2020Sampling","page":"Home","title":"Walker2020Sampling","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Walker2020Sampling.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Walker2020Sampling]","category":"page"}]
+}
