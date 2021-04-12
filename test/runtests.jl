2b2b62c7acdca70b2b88f153279e96e4db99f620
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-04-12 15:56:56 -0400

    Change the starting point in the first test

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using Test
 @testset "Walker2020Sampling.jl" begin
     s = Walker2020Sampler(lb = 0.1, ub = 1.0)
     rng = MersenneTwister(1)
-    y0, z0 = 1.0, 1.0
+    y0, z0 = 0.5, 0.5
     for i = 1:100
         y0, z0 = rand(rng, s, x -> logpdf(Exponential(1.0), x), y0, z0)
         @test y0 ≥ 0.0
