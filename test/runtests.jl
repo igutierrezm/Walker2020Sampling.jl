0da7f9d93fdbe03743f372c5a9bdfccc8a808315
Iván Gutiérrez <ivangutierrez1988@gmail.com>
2021-04-12 16:01:09 -0400

    Replace test distribution with a doubly-truncated normal

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,12 @@ using Random
 using Test
 
 @testset "Walker2020Sampling.jl" begin
-    s = Walker2020Sampler(lb = 0.1, ub = 1.0)
+    s = Walker2020Sampler(lb = -0.5, ub = +0.5)
     rng = MersenneTwister(1)
-    y0, z0 = 0.5, 0.5
-    for i = 1:100
-        y0, z0 = rand(rng, s, x -> logpdf(Exponential(1.0), x), y0, z0)
-        @test y0 ≥ 0.0
-        @test y0 ≤ 1.0 
+    y0, z0 = 0.0, 0.1
+    for i = 1:1000
+        y0, z0 = rand(rng, s, x -> logpdf(Normal(), x), y0, z0)
+        @test y0 ≥ -0.5
+        @test y0 ≤ +0.5
     end
 end
